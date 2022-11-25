bfcc9c98bc3d9c7787a9a2de01f2810c05bf7e64
Rafael Cassiano <MY_NAME@example.com>
2022-11-25 02:52:34 -0300

    finish

--- src/contexts/Auth.tsx
+++ src/contexts/Auth.tsx
@@ -1,4 +1,4 @@
-import { signInWithPopup } from "firebase/auth"
+import { signInWithPopup } from "firebase/auth";
 import {
   collection,
   doc,
@@ -7,72 +7,72 @@ import {
   query,
   setDoc,
   where,
-} from "firebase/firestore"
-import { useRouter } from "next/router"
+} from "firebase/firestore";
+import { useRouter } from "next/router";
 import {
   createContext,
   ReactNode,
   useContext,
   useEffect,
   useState,
-} from "react"
-import { auth, db, googleProvider } from "../config/firebase"
+} from "react";
+import { auth, db, googleProvider } from "../config/firebase";
 
 type User = {
-  name: string
-  email: string
-}
+  name: string;
+  email: string;
+};
 
 type ContextProps = {
-  user: User
-  handleLoginWithGoogle: () => void
-}
+  user: User;
+  handleLoginWithGoogle: () => void;
+};
 
 type ProviderProps = {
-  children: ReactNode
-}
+  children: ReactNode;
+};
 
-export const AuthContext = createContext({} as ContextProps)
+export const AuthContext = createContext({} as ContextProps);
 
 export const AuthProvider = ({ children }: ProviderProps) => {
-  const [user, setUser] = useState({} as User)
-  const { replace, pathname } = useRouter()
+  const [user, setUser] = useState({} as User);
+  const { replace, pathname } = useRouter();
 
   useEffect(() => {
     auth.onAuthStateChanged(async (user) => {
-      if (!user) return replace("/login")
+      if (!user) return replace("/login");
 
-      const docRef = doc(db, "users", user.uid)
-      const docSnap = await getDoc(docRef)
+      const docRef = doc(db, "users", user.uid);
+      const docSnap = await getDoc(docRef);
 
       if (!docSnap.exists()) {
         const newUser = {
           name: user.displayName as string,
           email: user.email as string,
           cards: [],
-        }
+        };
 
-        await setDoc(doc(db, "users", user.uid), newUser)
-        setUser(newUser)
-        return replace("/")
+        await setDoc(doc(db, "users", user.uid), newUser);
+        setUser(newUser);
+        return replace("/");
       }
 
-      setUser(docSnap.data())
-      pathname === "/login" && replace("/")
-    })
-  }, [])
+      setUser(docSnap.data());
+      pathname === "/login" && replace("/");
+    });
+  }, []);
 
   const handleLoginWithGoogle = async () =>
-    await signInWithPopup(auth, googleProvider)
+    await signInWithPopup(auth, googleProvider);
 
   return (
     <AuthContext.Provider value={{ user, handleLoginWithGoogle }}>
       {children}
     </AuthContext.Provider>
-  )
-}
+  );
+};
 
 export const useAuth = () => {
-  const context = useContext(AuthContext)
-  return context
-}
+  const context = useContext(AuthContext);
+  return context;
+};
