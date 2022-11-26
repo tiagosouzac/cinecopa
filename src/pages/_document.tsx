f8498c5ad873dfbf7dcf40283e18280f0a85e0da
unknown <Administrador@AEPNBW10020.aeplus.local>
2022-11-25 21:03:13 -0300

    fix styled components ssr config

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,26 +1,36 @@
-import Document, { DocumentContext, Html, Main, NextScript } from "next/document";
-import Head from "next/head";
-import { ServerStyleSheet } from "styled-components";
-import { getCssText } from "../styles";
+import Document, {
+  DocumentContext,
+  DocumentInitialProps,
+  Html,
+  Main,
+  NextScript,
+} from "next/document"
+import Head from "next/head"
+import { ServerStyleSheet } from "styled-components"
+import { getCssText } from "../styles"
 
 export default class MyDocument extends Document {
-  static async getInitialProps(ctx: DocumentContext) {
-    const sheet = new ServerStyleSheet();
-    const originalRenderPage = ctx.renderPage;
+  static async getInitialProps(
+    ctx: DocumentContext
+  ): Promise<DocumentInitialProps> {
+    const sheet = new ServerStyleSheet()
+    const originalRenderPage = ctx.renderPage
 
     try {
       ctx.renderPage = () =>
         originalRenderPage({
-          enhanceApp: (App: JSX.IntrinsicAttributes) => (props) => sheet.collectStyles(<App {...props} />),
-        });
+          enhanceApp: (App) => (props) =>
+            sheet.collectStyles(<App {...props} />),
+        })
 
-      const initialProps = await Document.getInitialProps(ctx);
+      const initialProps = await Document.getInitialProps(ctx)
       return {
         ...initialProps,
         styles: (
           <>
             {initialProps.styles}
             {sheet.getStyleElement()}
+
             <Html>
               <Head>
                 <link rel="preconnect" href="https://fonts.googleapis.com" />
@@ -28,13 +38,14 @@ export default class MyDocument extends Document {
                 <link
                   href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,300;1,400;1,500;1,700;1,900&display=swap"
                   rel="stylesheet"
-                ></link>
+                />
 
                 {/* SSR Stitches */}
-
-                <style id="stitches" dangerouslySetInnerHTML={{ __html: getCssText() }} />
+                <style
+                  id="stitches"
+                  dangerouslySetInnerHTML={{ __html: getCssText() }}
+                />
               </Head>
-
               <body>
                 <Main />
                 <NextScript />
@@ -43,9 +54,9 @@ export default class MyDocument extends Document {
             </Html>
           </>
         ),
-      };
+      }
     } finally {
-      sheet.seal();
+      sheet.seal()
     }
   }
 }
